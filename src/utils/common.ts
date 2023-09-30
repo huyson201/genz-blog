f922240e330c62f2854af551d5c9e759f5d2b508
huyson201vn <sonnguyen201.dev@gmail.com>
2023-09-30 12:23:55 +0700

    fix error url invalid

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,7 +1,7 @@
 export const getSiteURL = () => {
   return (
     process.env.SITE_URL ||
-    process.env.VERCEL_URL ||
+    `https://${process.env.VERCEL_URL}` ||
     `https://localhost:${process.env.PORT}`
   );
 };
